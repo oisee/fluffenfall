1a62489161a38b35b0ec0478f30d15c8729fe5e3
oisee <ooisee@gmail.com>
2016-05-26 20:38:48 +0500

    octaved fixed a bit

--- src/lib/OctavedGoRoundFluff.js
+++ src/lib/OctavedGoRoundFluff.js
@@ -1,29 +1,38 @@
 var FluffFrame = require("../lib/FluffFrame.js");
 var FluffPattern = require("../lib/FluffPattern.js");
 
-var OctavedGoRoundFluff = function (speed) {
+var OctavedGoRoundFluff = function (speedGoRound,speedOctaved) {
   var swap = [];
   swap[0] = new FluffFrame();
   swap[1] = new FluffFrame();
   swap[1].a.s = "c";
   swap[1].b.s = "a";
-  swap[1].b.sh = 1;
   swap[1].c.s = "b";
   swap[2] = new FluffFrame();
   swap[2].a.s = "b";
   swap[2].b.s = "c";
-  swap[2].b.sh = -1;
   swap[2].c.s = "a";
-  swap[2].c.sh = -1;
-  
+    
   var fpat = new FluffPattern();
   fpat.repeat = 99999;
   fpat.fframes = [];
+  
+  var octavedCounter = 0;
+  var octavedShift = -1;
 
   for (var ii = 0; ii < swap.length; ii++) {
-    var sw = swap[ii];
-    for (var i = 0; i < speed; i++) {
+    var sw = new FluffFrame();
+    var swi = swap[ii];
+    sw.a.s = swi.a.s ;
+    sw.b.s = swi.b.s ;
+    sw.c.s = swi.c.s ;
+    for (var i = 0; i < speedGoRound; i++) {
+      if (octavedCounter % speedGoRound  == 0) {
+        octavedShift = octavedShift * -1;
+      }
+      sw.a.sh = octavedShift;
       fpat.fframes.push(sw);
+      octavedCounter++;
     }
   }
   return [fpat];
--- src/test/test_createOctavedGoRoundFluff.js
+++ src/test/test_createOctavedGoRoundFluff.js
@@ -6,10 +6,12 @@ var opt = {
   noSoftStop: false,
 };
 
-var fpn = parseInt(process.argv[2]) || 4; //frames per note
-var fluff = new OctavedGoRoundFluff(fpn);
+var speedGoRound = parseInt(process.argv[2]) || 4; //go-round speed
+var speedOctaved = parseInt(process.argv[3]) || speedGoRound; //octaved speed
+
+var fluff = new OctavedGoRoundFluff(speedGoRound, speedOctaved);
 var fluffjson = JSON.stringify(fluff,null,2);
-fs.writeFileSync("./octavedGoRoundFluff."+fpn+".fluff.json" , fluffjson);
+fs.writeFileSync("./octavedGoRoundFluff."+speedGoRound+"."+speedOctaved+".fluff.json" , fluffjson);
 
 //console.log(fpn);
 //console.log(fluff);
